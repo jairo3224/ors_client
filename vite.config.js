c7b961dfdc03cac5a574e90e77210fe9719f831d
Jorby Jugalbot <jairo3224@gmail.com>
2026-06-03 22:04:52 +0800

    WIP: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 
 export default defineConfig({
-  base: '/ors_client-main/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
